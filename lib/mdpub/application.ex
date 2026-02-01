8b71626ae71c8186abef6d49fe8b6fa8afdb034d
Carey Janecka <careyjanecka@gmail.com>
2026-02-01 04:24:02 -0800

    fix: start correct supervisor children (Content + Web)

--- lib/mdpub/application.ex
+++ lib/mdpub/application.ex
@@ -1,28 +1,18 @@
 defmodule Mdpub.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
-    children =
-      if Mix.env() == :test do
-        []
-      else
-        port =
-          System.get_env("PORT", "4000")
-          |> String.to_integer()
+    port = (System.get_env("PORT") || "4000") |> String.to_integer()
 
-        [
-          Mdpub.Content,
-          {Bandit, plug: Mdpub.Web, scheme: :http, port: port}
-        ]
-      end
+    children = [
+      # Mdpub.Content owns the ETS cache and (optionally) starts the file watcher.
+      {Mdpub.Content, []},
+      {Bandit, plug: Mdpub.Web, scheme: :http, port: port}
+    ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Mdpub.Supervisor]
     Supervisor.start_link(children, opts)
   end
